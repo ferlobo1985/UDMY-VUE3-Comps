9e49c0134fd87f0238943773d83312c955893005
ferlobo1985 <fernando.lobo.prez@gmail.com>
2024-09-24 14:15:02 -0300

    02-App and Main
    02-App and Main

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>{{name}}</h1>
+</template>
+
+<script setup>
+    import {ref} from 'vue';
+    const name = ref("Hello world !!");
+</script>
+
+<style>
+    h1 {
+        color:red;
+    }
+</style>
