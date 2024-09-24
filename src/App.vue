e3af97c0908054ba4b38ef68ad00e18d94da31ae
ferlobo1985 <fernando.lobo.prez@gmail.com>
2024-09-24 18:49:33 -0300

    15-Using the Lifecycles
    15-Using the Lifecycles

--- src/App.vue
+++ src/App.vue
@@ -2,47 +2,17 @@
     <div>
         <app-header/>
         <div class="container">
-            <Cars/>
+            <Life v-if="showIt"/>
             <hr/>
-            <CarBrands>
-                <div>Content at the top</div>
-                <template v-slot:brands>
-                    <ul>
-                        <li v-for="(brand,index) in brands" :key="index">
-                            {{ brand }}
-                        </li>
-                    </ul>
-                </template>
-                <template v-slot:other>
-                    <div>Some other content</div>
-                </template>
-                <strong>Default slot</strong>
-            </CarBrands>
+            <button @click="showIt= !showIt">Toggle</button>
         </div>
     </div>
 </template>
 
 <script setup>
-    import { reactive, provide } from 'vue';
-    import Cars from './components/Cars/index.vue';
-    import CarBrands from './components/Cars/brands.vue';
-
-    const brands = reactive(['Mazda','Honda','Renault'])
-    const cars = reactive([
-        {model:'F9',brand:'Ferrari'},
-        {model:'911',brand:'Porsche'},
-        {model:'Tipo',brand:'Fiat'}
-    ])
-    
-
-    const updateCar = () => {
-        cars[0].model = 'Ford'
-    }
-
-    provide('cars',{
-        cars,
-        updateCar
-    })
+    import { ref } from 'vue';
+    import Life from '@/components/Life/index.vue'
+    const showIt = ref(true);
 
 </script>
 
