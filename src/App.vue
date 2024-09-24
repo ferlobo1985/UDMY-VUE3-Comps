b6be60731c70b07cd7782c19aebcd572ee1f04fd
ferlobo1985 <fernando.lobo.prez@gmail.com>
2024-09-24 17:56:34 -0300

    12-Slots
    12-Slots

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,13 @@
         <div class="container">
             <Cars/>
             <hr/>
-            <button @click="updateCar">Update</button>
+            <CarBrands>
+                <ul>
+                    <li v-for="(brand,index) in brands" :key="index">
+                        {{ brand }}
+                    </li>
+                </ul>
+            </CarBrands>
         </div>
     </div>
 </template>
@@ -12,12 +18,15 @@
 <script setup>
     import { reactive, provide } from 'vue';
     import Cars from './components/Cars/index.vue';
+    import CarBrands from './components/Cars/brands.vue';
 
+    const brands = reactive(['Mazda','Honda','Renault'])
     const cars = reactive([
         {model:'F9',brand:'Ferrari'},
         {model:'911',brand:'Porsche'},
         {model:'Tipo',brand:'Fiat'}
     ])
+    
 
     const updateCar = () => {
         cars[0].model = 'Ford'
@@ -41,5 +50,4 @@
         box-sizing: border-box;
         padding: 20px;
     }
-
 </style>
---
+++ src/components/Cars/brands.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="car_brands">
+        <h3>Car brands</h3>
+        <slot></slot>
+    </div>
+</template>
+
+<style scoped>
+    .car_brands {
+        border: 1px solid #2196f3;
+        padding: 10px 20px;
+    }
+</style>
