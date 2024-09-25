fdb25d25c5fcb055e8bd47612b157b05decae425
ferlobo1985 <fernando.lobo.prez@gmail.com>
2024-09-25 13:40:40 -0300

    17-Keep alive
    17-Keep alive

--- src/components/Players/steve.vue
+++ src/components/Players/steve.vue
@@ -1,3 +1,10 @@
 <template>
     <h3>Steve is on</h3>
 </template>
+
+<script setup>
+    import { onUnmounted } from 'vue';
+    onUnmounted(()=>{
+        console.log('Steve onUnmounted')
+    })
+</script>
