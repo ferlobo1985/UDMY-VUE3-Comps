d334b5a057135539832973fd1730416ad48a41fb
ferlobo1985 <fernando.lobo.prez@gmail.com>
2024-09-25 14:25:31 -0300

    21-Local custom directive
    21-Local custom directive

--- src/components/Directives/index.vue
+++ src/components/Directives/index.vue
@@ -1,11 +1,22 @@
 <template>
-    <div v-awesome.red.big="textToRender"></div>
+    <!-- <div v-awesome.red.big="textToRender"></div>
     <button @click="textToRender = 'Something else'">
         Trigger update
-    </button>
+    </button> -->
+    <hr/>
+    <div v-user-lastname="lastname"></div>
 </template>
 
 <script setup>
     import { ref } from 'vue';
-    const textToRender = ref('Some text')
+    const textToRender = ref('Some text');
+    const lastname = ref('Jones')
+
+    const vUserLastname = {
+        beforeMount(el,binding){
+            el.innerHTML = binding.value;
+            el.style.color = 'red';
+        }
+    }
+
 </script>
